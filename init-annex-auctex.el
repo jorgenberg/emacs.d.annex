8ac9638c8e8226e83c9df41131c09c9375c41465
tberg <tjberg@pm.me>
2021-03-11 14:53:19 +1300

    added auxtex stuff

--- init-annex-auctex.el
+++ init-annex-auctex.el
@@ -8,8 +8,11 @@
 
 (setq TeX-auto-save t)
 (setq TeX-parse-self t)
+(setq-default TeX-master nil)
 (add-hook 'LaTeX-mode-hook '(lambda ()
-                              (turn-on-flyspell)))
+                              (turn-on-flyspell)
+                              (LaTeX-math-mode)
+                              (visual-line-mode)))
 
 (provide 'init-annex-auctex)
 ;;; init-annex-auctex.el ends here
